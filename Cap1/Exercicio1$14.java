95dafe9877e03d9562eb568e9a14984ae1460e22
diegocortez86 <163600959+diegocortez86@users.noreply.github.com>
2024-09-22 13:42:04 -0300

    Update Exercicio1$14.java

--- Cap1/Exercicio1$14.java
+++ Cap1/Exercicio1$14.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
+
 public class Exercicio1$14 {
 
     public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor do lado do quadrado: ");
+        double lado = input.nextDouble();  
+
+      
+        double perimetro = lado * 4;
+        double area = lado * lado;
+
         
-    double lado = 5.5 ;   
+        System.out.printf("Perímetro = %.2f\n", perimetro);
+        System.out.printf("Área = %.2f\n", area);
 
-    double perimetro = lado * 4;
-    double area = lado * lado;
-
-    System.out.printf("Perimetro = %.2f  \n", perimetro);
-    System.out.printf("Area = %.2f \n", area);
-
-
-
-
+        input.close();
     }
-    
 }
